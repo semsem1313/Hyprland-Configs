a041d4d31704a3d27f33bb1a68d60432986a6510
semsem1313 <samarth13007@gmail.com>
2026-03-05 19:35:42 +0530

    Update powermenu.sh
    make this executable by:
    chmod +x ~/.config/waybar/scripts/powermenu.sh

--- themes/waybar/powermenu.sh
+++ themes/waybar/powermenu.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-chosen=$(printf "󰐥 Shutdown\n󰜉 Reboot\n󰤄 Sleep" | wofi --dmenu --width 200 --height 200)
+# Toggle behavior
+if pgrep -x wofi > /dev/null; then
+    pkill wofi
+    exit 0
+fi
 
-case "$chosen" in
-    *Shutdown) systemctl poweroff ;;
-    *Reboot) systemctl reboot ;;
-    *Sleep) systemctl suspend ;;
-esac
+printf "󰐥  Shutdown\n󰜉  Reboot\n󰤄  Sleep" | \
+wofi --dmenu \
+     --width 220 \
+     --height 170 \
+     --location 1 \
+     --xoffset 10 \
+     --yoffset 15 \
+     --prompt "" \
+     --style ~/.config/wofi/style.css | while read -r choice; do
+
+    case "$choice" in
+        *Shutdown) systemctl poweroff ;;
+        *Reboot) systemctl reboot ;;
+        *Sleep) systemctl suspend ;;
+    esac
+
+done
